a3f88809a611e7f9b41611013109a8a43e114bdc
Nikita Shevyakov <nshevyakov@servicechannel.com>
2023-07-03 16:38:43 +0300

    main - CRUD operations were added

--- MangoRestaurant/Mango.Web/Controllers/ProductController.cs
+++ MangoRestaurant/Mango.Web/Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using Mango.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Mango.Web.Controllers
 {
@@ -22,6 +24,81 @@ namespace Mango.Web.Controllers
                 list = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(responce.Result));
             }
             return View(list);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var responce = await _productService.CreateProductAsync<RespoceDTO>(model);
+                if (responce != null && responce.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            var responce = await _productService.GetProductByIdAsync<RespoceDTO>(productId);
+            if (responce != null && responce.IsSuccess)
+            {
+                var model = JsonConvert.DeserializeObject<ProductDTO>(Convert.ToString(responce.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductEdit(ProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var responce = await _productService.UpdateProductAsync<RespoceDTO>(model);
+                if (responce != null && responce.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int productId)
+        {
+            var responce = await _productService.GetProductByIdAsync<RespoceDTO>(productId);
+            if (responce != null && responce.IsSuccess)
+            {
+                var model = JsonConvert.DeserializeObject<ProductDTO>(Convert.ToString(responce.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductDelete(ProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var responce = await _productService.DeleteProductAsync<RespoceDTO>(model.ProductId);
+                if (responce.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+
+            return View(model);
         }
     }
 }
